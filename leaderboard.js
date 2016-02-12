d4492da39e3b82555f6a450f0eceac4b837eec69
Yanis <yanis.urbis@gmail.com>
2016-02-12 11:29:29 +0300

    First DB.

--- leaderboard.js
+++ leaderboard.js
@@ -1,1 +1,11 @@
 console.log('Hello World')
+
+PlayersList = new Mongo.Collection('players')
+
+// we have access to this collection on the client
+// insert, update, remove, find
+
+// PlayersList.find()
+// PlayersList.find().fetch()
+// PlayersList.find({ name: "John" })
+// PlayersList.find().count()
