01213bac839cfeae173cc32ea34d206bdc98fbdc
Yanis <yanis.urbis@gmail.com>
2016-02-12 11:43:39 +0300

    Basic template and helper.
    The old way of doing helpers.

--- leaderboard.js
+++ leaderboard.js
@@ -9,3 +9,16 @@ PlayersList = new Mongo.Collection('players')
 // PlayersList.find().fetch()
 // PlayersList.find({ name: "John" })
 // PlayersList.find().count()
+
+if(Meteor.isClient){
+    console.log("Hello client");
+
+    // old way of doing stuff
+    Template.leaderboard.player = function() {
+      return 'Some other text'
+    }
+}
+
+if(Meteor.isServer){
+    console.log("Hello server");
+}
